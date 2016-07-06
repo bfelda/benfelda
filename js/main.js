6cd1e9979c880c367aea3470d86ffd31b6fc5a78
bfelda <bfelda@gmail.com>
2016-07-05 22:00:26 -0500

    Finished up the site

--- js/main.js
+++ js/main.js
@@ -10,18 +10,18 @@
         menu: '#fp-nav',
         lockAnchors: false,
         anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'footer'],
-        navigation: true,
-        navigationPosition: 'left',
+        //navigation: true,
+        //navigationPosition: 'left',
         showActiveTooltip: false,
         slidesNavigation: true,
         slidesNavPosition: 'bottom',
 
         //Scrolling
         css3: true,
-        scrollingSpeed: 500,
+        scrollingSpeed: 750,
         autoScrolling: true,
         fitToSection: true,
-        fitToSectionDelay: 1000,
+        fitToSectionDelay: 750,
         scrollBar: false,
         easing: 'easeOutCubic',
         easingcss3: 'ease',
@@ -76,7 +76,8 @@
                     'height': '80%',
                     'width':'100%', 
                     'margin': height * .15 + 'px 0 0 0',
-                    'background':'rgba(0,0,0,0.3)'
+                    'background':'rgba(0,0,0,0.3)',
+                    'z-index':1
                 });
                 break;
                 case 2:
@@ -85,42 +86,52 @@
                     'height': '100%', 
                     'width':'35%',  
                     'margin': height + 'px 25% 0 0',
+                    'z-index':1,
                     'background':'rgba(19,48,77,0.3)'
                 }); 
-                // var ballWidth = height * 1.3,
-                //     ballMarginLeft = (width/2) - (ballWidth/2);
-                // ball.css({
-                //     'border-radius':'50%', 
-                //     'height': '130%', 
-                //     'width':ballWidth,  
-                //     'margin':height * .85 + 'px 0 0 ' + ballMarginLeft + 'px',
-                //     'background':'rgba(0,0,0,0.5)'
-                // });
                 break;
                 case 3:
+                var ballWidth = height * 1.3,
+                    ballMarginLeft = width/2;
+             //$('.textContainer').css({'width': ballWidth / 2, 'left': ballWidth / 2});
+                    //ballMarginLeft = (width/2) - (ballWidth/2);
                 ball.css({
-                    'border-radius':'0', 
-                    'height': '50%', 
-                    'width':'100%',  
-                    'margin': height * 2.25 + 'px 0 0 0',
-                    'background':'rgba(0,0,0,0.7)'
-                });                
+                    'border-radius':'50%', 
+                    'height': '130%', 
+                    'width':ballWidth,  
+                    'margin':height * 1.85 + 'px 0 0 ' + ballMarginLeft + 'px',
+                    'z-index':2,
+                    'background':'rgba(255,255,255,0.8)'
+                });
+
+                //Slider background
+                // ball.css({
+                //     'border-radius':'0', 
+                //     'height': '50%', 
+                //     'width':'100%',  
+                //     'margin': height * 2.25 + 'px 0 0 0',
+                //     'background':'rgba(0,0,0,0.7)'
+                // });                
                 break;
                 case 4:
+                $('.slide2TextContainer').css({'color':'white', 'background':'rgba(255,255,255,0)'});
                 ball.css({
                     'border-radius':'0',
-                    'height': '50%', 
-                    'width':'80%',  
-                    'margin':height * 3.25 + 'px 0 0 10%',
+                    'height': '60%', 
+                    'width':'50%',  
+                    'margin':height * 3.2 + 'px 0 0 0',
+                    'z-index':1,
                     'background':'rgba(0,0,0,0.7)'
                     });                                
                 break;
                 case 5:
+                $('.slide2TextContainer').css({'color':'black', 'background':'rgba(255,255,255,.7)'});
                 ball.css({
                     'border-radius':'0', 
                     'height': '100px', 
                     'width':'100%', 
                     'margin':height * 4 + 'px 0 0 0',
+                    'z-index':1,
                     'background':'rgba(0,0,0,0.7)'
                 });
                 break;
